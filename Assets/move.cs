19ae7b08a733753d3441d6eb98fa203a1f0358c4
rudgnsgl <yong88asas@gmail.com>
2025-03-19 18:13:19 +0900

    first commit

---
+++ Assets/move.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private int speed = 10;
+    public int jumpPower = 20;
+    public int jumpCnt = 0;
+
+    private Vector3 movement;
+
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+
+        movement.Normalize();
+
+        rigid.velocity = movement * speed;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCnt < 1)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            jumpCnt++;
+        }
+        Physics.gravity = new Vector3(0, -50f, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCnt = 0;
+        }
+    }
+}
